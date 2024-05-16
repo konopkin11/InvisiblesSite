86df253f800e8a734278daf360b30df44415e4ad
evgenijkonopkin <konopkin11@gmail.com>
2024-05-16 22:57:57 +0300

    asd

--- pages/cursov/game_level2/task4.js
+++ pages/cursov/game_level2/task4.js
@@ -60,7 +60,6 @@ animals.forEach(animal => {
   }
 });
 
-
 let score = 0;
 localStorage.setItem("score", localStorage.getItem("score") || 0);
 
@@ -73,20 +72,20 @@ function showNextImage() {
   if (index >= animals.length) return;
   let img = document.createElement('img');
 
-  img.src = "../assets/" + animals[index].fileName;
+ 
   index += 1;
-
+  img.src = "../assets/" + animals[index].fileName;
   img.style.maxWidth = '150px';
 
   center.appendChild(img);
 }
 window.onkeydown = function (event) {
   if (center.firstChild) {
-    console.log(animals[index-1].fileName)
+    console.log(animals[index].fileName)
     if (event.key === 'ArrowLeft') {
       left.appendChild(center.firstChild);
 
-      if (animals[index-1].name.startsWith(letterLeft)) {
+      if (animals[index].name.startsWith(letterLeft)) {
         score += 15;
         document.getElementById("score").innerHTML = " Cчет: " + score;
         clickerAmountLeft += 1;
@@ -104,7 +103,7 @@ window.onkeydown = function (event) {
     } else if (event.key === 'ArrowRight') {
       right.appendChild(center.firstChild);
 
-      if (animals[index-1].name.includes(letterRight)) {
+      if (animals[index].name.includes(letterRight)) {
         score += 15;
         document.getElementById("score").innerHTML = " Cчет: " + score;
         clickerAmountRight += 1;
@@ -122,7 +121,7 @@ window.onkeydown = function (event) {
       showNextImage();
     } else if (event.key === 'ArrowDown') {
 
-      if (animals[index-1].name.includes(letterRight) || animals[index-1].name.startsWith(letterLeft)) {
+      if (animals[index].name.includes(letterRight) || animals[index].name.startsWith(letterLeft)) {
         alert("Вы проиграли. Счет: " + score);
         window.location.href = "../finalScene/index.html";
 
@@ -140,49 +139,4 @@ window.onload = showNextImage;
 
 
 
-
-
-// let img = document.createElement('img');
-// img.src = "../assets/" + animals[i].fileName;
-// img.className = 'falling';
-// img.style.maxWidth = '150px';
-// img.style.left = Math.random() * window.innerWidth + 'px';
-// img.style.animationDuration = Math.max(5, Math.random() * 10) + 's';
-// //подсказка
-// if(animals[i].name.startsWith(letter)){
-//   img.style.border = '2px solid red';
-// }
-
-// //
-// img.onclick = function () {
-//   this.parentNode.removeChild(this);
-//   if (animals[i].name.startsWith(letter)) {
-//     score += 10;
-//     document.getElementById("score").innerHTML = " Cчет: " + score;
-//     clickerAmount += 1;
-//     localStorage.setItem("score", score);
-//     if (clickerAmount == totalAmount) {
-//       alert("Вы выиграли!" + "Cчет: " + score);
-//       document.getElementById("score").innerHTML = " Cчет: " + score;
-//       window.location.href = "../index.html";
-//     }
-//   } else {
-//     alert("Вы проиграли. Счет: " + score);
-//     window.location.href = "../finalScene/index.html"; //todo отдельная страница финала
-//   }
-
-// };
-// img.addEventListener('animationend', () => {
-//   if (!animals[i].name.startsWith(letter)) {
-//     img.parentNode.removeChild(img);
-//   } else {
-//     alert("Вы проиграли. Счет: "+ score);
-//     window.location.href = "../finalScene/index.html"; //todo отдельная страница финала
-
-//   }
-
-// });
-// document.body.appendChild(img);
-// //sleep for 2 secs
-// await sleep(1000);
 
